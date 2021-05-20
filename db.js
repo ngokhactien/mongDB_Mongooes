224f9f60a5b244ecdd078ef1ee6346f54ee8c389
khactien <khactien9d@gmail.com>
2021-05-20 10:24:57 +0700

    first-1

---
+++ db.js
@@ -0,0 +1,41 @@
+//1 repuire mongooes 
+const mongoose = require('mongoose');
+//2 connect
+mongoose.connect('mongodb://localhost/myDataBase', {useNewUrlParser: true, useUnifiedTopology: true});  //myDataBase là của db database (use myDataBase)
+//3 tao schema    		// khai báo những cái field có trong object của chúng ta , lưu gì database ghi vào 
+const userSchema = new mongoose.Schema({
+    name : String ,
+    age :Number 
+});
+// //4 tao  model để kết nối dữ liệu 
+
+ const user = mongoose.model('user' , userSchema);    //user là tên của model 
+//const user = mongoose.model('user' , userSchema , "shopping" );    //user là tên của model ,  "shopping"  là tên collection
+
+// CRUD
+
+// user.create([
+//     {name :'tao1', age :231},
+//     {name :'Tam1', age :17 }
+// ], (err , res)=>{
+//     console.log(res);
+// });
+
+// user.find().exec((err , users)=>{
+//     console.log(users);
+// });
+
+// cách 1
+// user.update({name : 'khactien1'} , {name: 'khactien12'})
+// .exec((err , result )=>{
+//     console.log(result);
+// });
+// cách 2
+// user.update({name : 'khactien12'} , {name: 'khactien'} , (err, res)=>{
+//     console.log(res);
+// });
+
+// user.remove({name: 'khactien'})
+// .exec((err , result )=>{
+//     console.log(result);
+// });
